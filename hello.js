52220ddb3a884ee29bca0fc5e49106ea1abdf578
Siya10 <leyanesiyabonga00@gmail.com>
2025-03-21 09:38:58 +0200

    Exercise 8

--- hello.js
+++ hello.js
@@ -1,9 +1,17 @@
 'use strict' // don't forget this, it won't be shown in future code samples
 
-const numbers = [76, 55.7, 89, 37.5, 61]
-numbers.push(19) // puts 19 at the end of the array
-numbers.unshift(61) // puts 61 at the start of the list
-numbers[1] = 12 // Changes the value at index 1, ie 76 becomes 12
-numbers.splice(3, 1, 99) // Goes to index 3, and replaces 1 element, starting at index 3, with the value 99
-console.log(numbers) // 61, 12, 55.7, 99, 37.5, 61, 19
-console.log(numbers[7]) // undefined
+const map = function (functionToApply, array) {
+  const newArray = []
+  array.forEach(function (num) { newArray.push(functionToApply(num)) })
+  return newArray
+}
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+
+const squaringFunction = function (number) { return number * number }
+const newNumbers = map(squaringFunction, numbers)
+console.log(newNumbers)
+
+const newNumbersMap = numbers.map(squaringFunction)
+console.log(newNumbersMap)
