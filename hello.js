f11f1507829f296d529636607ecc667235fb1b2e
Siya10 <leyanesiyabonga00@gmail.com>
2025-03-21 09:27:35 +0200

    Exercise 6

--- hello.js
+++ hello.js
@@ -1,29 +1,35 @@
 'use strict' // don't forget this, it won't be shown in future code samples
 
-const ELEN4010 = {
-  courseCode: 'ELEN4010'
+const account = {
+  balance: 0,
+  withdrawals: 0,
+  deposits: 0,
+
+  deposit: function (amount) {
+    if (typeof (amount) !== 'number' || amount < 0) { // Ensure that we only handle positive numbers
+      console.log('Could not execute transaction') // In actual world you'd use exceptions
+    } else {
+      this.balance += amount
+      this.deposits += amount
+    }
+  },
+
+  withdraw: function (amount) {
+    if (typeof (amount) !== 'number' || amount > this.balance) { // Ensure that we only handle numbers, and the amount withdrawn is less than the balance
+      console.log('Could not execute transaction')
+    } else {
+      this.balance -= amount
+      this.withdrawals += amount
+    }
+  },
+
+  getSummary: function () {
+    return `This account has a balance of R ${this.balance}. There have been deposits totalling R ${this.deposis} and withdrawals totalling R${this.withdrawals}.`
+  }
 }
 
-const ELEN3009 = {
-  courseCode: 'ELEN3009'
-}
-
-const ELEN2003 = {
-  courseCode: 'ELEN2009'
-}
-
-const ELEN1003 = {
-  courseCode: 'ELEN1009'
-}
-
-const getCourseSummary = function (course) {
-  let yos = ''
-  if (course.courseCode.includes('40')) { yos = '4th' } else if (course.courseCode.includes('30')) { yos = '3rd' } else if (course.courseCode.includes('20')) { yos = '2nd' } else { return 'Course not found.' }
-
-  return `${course.courseCode} is offered in the ${yos} year of study.`
-}
-
-console.log(getCourseSummary(ELEN4010))
-console.log(getCourseSummary(ELEN3009))
-console.log(getCourseSummary(ELEN2003))
-console.log(getCourseSummary(ELEN1003))
+account.deposit(100)
+account.deposit(300)
+account.withdraw(800)
+account.withdraw(150)
+console.log(account.getSummary())
