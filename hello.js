fb33c073e8eb9720e60516accd75a8e10561d1ed
Siya10 <leyanesiyabonga00@gmail.com>
2025-03-21 09:26:40 +0200

    Exercise 4

--- hello.js
+++ hello.js
@@ -1,8 +1,7 @@
 'use strict' // don't forget this, it won't be shown in future code samples
-let a = 'hello'
-console.log(`a = ${a}, "a" is ${typeof a}`)
-a = 5
-a += 2
-console.log(`a = ${a}, "a" is ${typeof a}`)
-a = true
-console.log(`a = ${a}, "a" is ${typeof a}`)
+
+const toCelcius = function (tempInFarenheit) {
+  return (tempInFarenheit - 32) * (5 / 9)
+}
+
+console.log(toCelcius(90))
