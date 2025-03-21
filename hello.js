3311ae255096afbe5e1e9b2d441382fac4496c79
Siya10 <leyanesiyabonga00@gmail.com>
2025-03-21 10:20:52 +0200

    Exercise 11

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
 'use strict' // don't forget this, it won't be shown in future code samples
+// Run npm install dolor first
+const dolor = require('dolor')
+const fs = require('fs')
+
+const fileContents = dolor.paragraphs(100)
+fs.writeFile('./GeneratedOutputFile.txt', fileContents, (error) => {
+  if (error) throw error
+})
