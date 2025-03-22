4e62056b5274e0adc6dff9de5115a0dca7d7752f
Siya10 <leyanesiyabonga00@gmail.com>
2025-03-22 10:36:53 +0200

    Routing 14.1

---
+++ index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+app.listen(3000)
+console.log('App running on port 3000')
